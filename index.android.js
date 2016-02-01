5a8a700c842a3aafb7e87cd368823a79d7188e1d
cqm <1025760508@qq.com>
2016-02-01 18:03:56 +0800

    hello

---
+++ index.android.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+    AppRegistry,
+    Component,
+    StyleSheet,
+    Text,
+    View,
+    Navigator
+} from 'react-native';
+
+import main from './mainAndroid.js';
+
+class myDate extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            date: new Date()
+        }
+    };
+
+    render() {
+        return (
+            <Navigator
+                initialRoute={{ name: 'main', component: main, params: {date: this.state.date}}}
+                configureScene={() => {
+                    return Navigator.SceneConfigs.FloatFromLeft;
+                }}
+                renderScene={(route, navigator)=>{
+                        let Component = route.component;
+                        if(route.component) {
+                        return <Component {...route.params} navigator={navigator} />
+                    }
+                }}
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+AppRegistry.registerComponent('myDate', () => myDate);
